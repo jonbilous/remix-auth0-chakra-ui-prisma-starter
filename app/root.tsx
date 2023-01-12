e858c6b4410b8bb7f9582a6cbc15ba97a005dfe2
Jon Bilous <jon@noirgallery.co>
2023-01-12 07:36:05 -0500

    name from brand

--- app/root.tsx
+++ app/root.tsx
@@ -2,12 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { DataFunctionArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import brand from "brand";
 import EmotionDocumentWrapper from "./chakra/components/EmotionDocumentWrapper";
 import { getUserProfile } from "./utils/auth/profile.server";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: brand.name,
   viewport: "width=device-width,initial-scale=1",
 });
 
